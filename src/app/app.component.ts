69f251b35fab177f6620cce001630cc21a5c6af7
lufecrx <luizfelipecrx@gmail.com>
2024-02-26 19:02:36 -0300

    add projects file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { WikiapiService } from './services/wikiapi.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wikipedia-query';
+  title = 'wikipedia-restapi';
+
+  articles: any[] = [];
+  selectedArticle: any = '';
+
+  constructor(private wikiService: WikiapiService) {}
+
+  search(term: string) {
+    this.wikiService.search(term).subscribe((response: any) => {
+      this.wikiService.getArticlesDetails(response.query.search).subscribe((details: any[]) => {
+        this.articles = response.query.search.map((result: any, index: number) => ({
+          title: result.title,
+          snippet: result.snippet,
+          fullurl: details[index].fullurl,
+        }));
+        this.selectedArticle = null;
+      });
+    });
+  }
 }
